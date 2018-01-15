e4318f252800649d9ed539f0917f86b28c3bdfac
Bryan Veloso <bryan@avalonstar.com>
2018-01-14 16:38:08 -0800

    Add a Caster model.

---
+++ casters/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Caster(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=255, blank=True)
+    kamioshi = models.CharField(max_length=255)
+    oshi_overall = models.CharField(max_length=255)
+    oshi_current = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
+    
