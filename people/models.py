9d4d53ee1e9d60d637e99996cd461dfd63c4a024
Bryan Veloso <bryan@avalonstar.com>
2018-01-25 20:19:43 -0800

    Add an affiliation field to Person.

--- people/models.py
+++ people/models.py
@@ -10,6 +10,8 @@ class Person(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=60)
     birthday = BirthdayField()
+    affiliation = models.CharField(max_length=255, blank=True)
+
     objects = BirthdayManager()
 
     def __str__(self):
