01c8772d7a37a576a65682e26ac4073a7715f241
Bryan Veloso <bryan@avalonstar.com>
2018-01-29 21:37:13 -0800

    Add a biography field to Casters.

--- casters/models.py
+++ casters/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Caster(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=255, blank=True)
+    biography = models.TextField(blank=True)
     kamioshi = models.CharField(max_length=255, blank=True)
     oshi_overall = models.CharField('All-Time Favorites', max_length=255, blank=True)
     oshi_current = models.CharField('Current Favorites', max_length=255, blank=True)
