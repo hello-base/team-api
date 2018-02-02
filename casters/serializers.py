2cc25961fafd26e8ac3805d26046c2601297a1f4
Bryan Veloso <bryan@avalonstar.com>
2018-02-01 21:29:09 -0800

    Add order to the API.

--- casters/serializers.py
+++ casters/serializers.py
@@ -7,10 +7,12 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     nickname = serializers.CharField(source='caster.nickname')
     biography = serializers.CharField(source='caster.biography')
     kamioshi = serializers.CharField(source='caster.kamioshi')
+    order = serializers.IntegerField(source='caster.order')
     oshi_overall = serializers.CharField(source='caster.oshi_overall')
+    oshi_current = serializers.CharField(source='caster.oshi_current')
     oshi_current = serializers.CharField(source='caster.oshi_current')
 
     class Meta:
         model = User
         fields = ('username', 'nickname', 'biography', 'kamioshi',
-            'oshi_overall', 'oshi_current')
+            'order', 'oshi_overall', 'oshi_current')
