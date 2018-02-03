13cc44de5c417d420859f3bb98f786dfb138a22c
Bryan Veloso <bryan@avalonstar.com>
2018-02-03 14:20:30 -0800

    Custom ordering to respect... order.

--- casters/views.py
+++ casters/views.py
@@ -9,3 +9,7 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def filter_queryset(self, queryset):
+        queryset = super(UserViewSet, self).filter_queryset(queryset)
+        return queryset.order_by('caster__order')
